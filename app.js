8e54b539b50c656c6780f537495c498942da805d
Lucas Krul <l3k.scorp@gmail.com>
2019-05-09 00:01:07 -0300

    Criando o jogo

---
+++ app.js
@@ -0,0 +1,90 @@
+new Vue({
+    el: '#app',
+    data: {
+        running: false,
+        playerLife: 100,
+        monsterLife: 100,
+    },
+    computed: {        
+        colorPlayerLife1() {
+            if(this.playerLife > 70) {
+                return 'green lighten-3'
+            }
+            if(this.playerLife <= 70 &&  this.playerLife >= 30){
+                return 'yellow lighten-3'
+            }
+            if(this.playerLife < 30 ){
+                return 'red lighten-3'
+            }
+        },
+        colorPlayerLife2() {
+            if(this.playerLife > 70) {
+                return 'green darken-3'
+            }
+            if(this.playerLife <= 70 &&  this.playerLife >= 30){
+                return 'yellow darken-1'
+            }
+            if(this.playerLife < 30 ){
+                return 'red darken-1'
+            }
+        },
+        colorMonsterLife1() {
+            if(this.monsterLife > 70) {
+                return 'green lighten-3'
+            }
+            if(this.monsterLife <= 70 &&  this.monsterLife >= 30){
+                return 'yellow lighten-3'
+            }
+            if(this.monsterLife < 30 ){
+                return 'red lighten-3'
+            }
+        },
+        colorMonsterLife2() {
+            if(this.monsterLife > 70) {
+                return 'green darken-3'
+            }
+            if(this.monsterLife <= 70 &&  this.monsterLife >= 30){
+                return 'yellow darken-1'
+            }
+            if(this.monsterLife < 30 ){
+                return 'red darken-1'
+            }
+        },
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
+    },
+    methods: {
+        startGame() {
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+        },
+        attack(special) {
+            this.hurt('monsterLife', 5, 10, special)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        hurt(atr, min, max, special) {
+            const plus = special ? 5 : 0
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[atr] = Math.max(this[atr] - hurt, 0)
+        },
+        healAndHurt() {
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false)
+        },
+        heal(min, max) {
+            const heal = this.getRandom(min, max)
+            this.playerLife = Math.min(this.playerLife + heal, 100)
+        },
+        getRandom(min, max) {
+            const value = Math.random() * (max - min) + min
+            return Math.round(value)
+        }
+    },
+    watch: {
+        hasResult(value){
+            if(value) this.running = false
+        }
+    },
+})
